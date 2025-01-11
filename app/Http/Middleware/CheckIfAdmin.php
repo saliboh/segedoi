0bb9a6616e4fba710d7fdc6b3de0bb8dc6c1a082
Andrean <shufhadha@gmail.com>
2025-01-11 11:56:52 +0800

    only admin can login

--- app/Http/Middleware/CheckIfAdmin.php
+++ app/Http/Middleware/CheckIfAdmin.php
@@ -27,8 +27,7 @@ class CheckIfAdmin
      */
     private function checkIfUserIsAdmin($user)
     {
-        // return ($user->is_admin == 1);
-        return true;
+        return ($user->isAdmin() == 1);
     }
 
     /**
@@ -60,6 +59,8 @@ class CheckIfAdmin
         }
 
         if (! $this->checkIfUserIsAdmin(backpack_user())) {
+            //logout user
+            backpack_auth()->logout();
             return $this->respondToUnauthorizedRequest($request);
         }
 
